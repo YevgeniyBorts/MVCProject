d345e35456e7f5e12493486c1332578e339140e2
Eugene Borts <eugene_borts@daytonastate.edu>
2019-12-18 00:26:22 -0500

    Entered first project into projects list.

---
+++ MVCProject/Controllers/ProjectController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListProjects()
+        {
+            List<Models.ProjectModel> projects = new List<Models.ProjectModel>();
+
+            projects.Add(new Models.ProjectModel { Name = "MVC Project", Classification = "ASP.NET MVC", Description = "My first MVC project.", Reference = "https://github.com/YevgeniyBorts/MVCProject", Complete = false, Version = 1.0 }); 
+
+            return View();
+        }
+    }
+}
---
+++ MVCProject/Models/ProjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class ProjectModel
+    {
+
+        public string Name { get; set; }
+        public string Classification { get; set; }
+        public string Description { get; set; }
+        public string Reference { get; set; }
+        public bool Complete { get; set; }
+        public double Version { get; set; }
+
+    }
+}
